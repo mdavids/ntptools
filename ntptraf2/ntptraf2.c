751469045674b5dc2127e84fef99d150c24cd8b4
Marco Davids <marco.davids@sidn.nl>
2021-08-04 22:36:46 +0200

    Another idea

--- ntptraf2/ntptraf2.c
+++ ntptraf2/ntptraf2.c
@@ -22,6 +22,7 @@
  *  - count IP_TTL ? Could be interesting for anycast catchment measurments
  *  - NTS (RFC8915) counters?
  *  - Better layout? Other colours?
+ *  - Reset counter butten ('r' key ?) that will reset counters to 0.
  *  - Daemonize option to turn it into a Prometheus exporter for Grafana?
  */
 
