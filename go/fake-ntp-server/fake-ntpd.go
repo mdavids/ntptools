9f6fa83462e5f8f3fef79cb12119ef0440b0fc3f
Marco Davids <marco.davids@sidn.nl>
2025-07-13 10:15:44 +0200

    Update fake-ntpd.go

--- go/fake-ntp-server/fake-ntpd.go
+++ go/fake-ntp-server/fake-ntpd.go
@@ -201,7 +201,8 @@ func main() {
 
         cfg := loadConfig(*configPath)
 
-        timeFormat := "2006-01-02 15:04:05 MST"
+        //timeFormat := "2006-01-02 15:04:05 MST"
+        timeFormat := "Jan _2 2006  15:04:05.00000000 (MST)"
 
         addr := net.UDPAddr{
                 Port: cfg.Port,
